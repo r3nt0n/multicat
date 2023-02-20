05ad3cab0d946d03580d976dd24eb2e60f346dae
r3nt0n <r3nt0n@protonmail.com>
2023-02-20 15:16:04 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,93 @@
+<!-- PROJECT SHIELDS -->
+![[GPL-3.0 License](https://github.com/r3nt0n)](https://img.shields.io/badge/license-GPL%203.0-brightgreen.svg)
+![[Python 3](https://github.com/r3nt0n)](http://img.shields.io/badge/python-3-blue.svg)
+![[Version 0.1.1](https://github.com/r3nt0n)](http://img.shields.io/badge/version-0.1.1-orange.svg)
+
+
+<br />
+<div align="center">
+  <h3 align="center">multicat</h3>
+  <p align="center">
+    Multithread reverse shell listener
+  </p>
+</div>
+
+
+<!-- ABOUT THE PROJECT -->
+## Use cases
+
+<p align="center"><img src="https://github.com/r3nt0n/multicat/blob/master/img/mc.gif" /></p>  
+
+Sometimes you have to spread the same payload to multiple targets and don't know how many clients will try to connect back. Multicat allow to setup a **listener capable of handling multiple connections on the same port** and **interact with each client on a separate session**.  
+
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+```commandline
+
+Usage: mc.py [-h] [-p] [-m] [-t]
+
+-h, --help           show this help message and exit
+-p , --port          port to listen (default: 28000)
+-m , --max-clients   max number of new clients to queue before establish connection (default: 5)
+-t , --timeout       connections timeout (default: 10)
+
+```
+
+Available commands in general menu context:
+```commandline
+COMMAND         DESCRIPTION
+------------------------------------
+HELP            List available commands
+SESSIONS        List established sessions
+START <id>      Interact with a client
+CLOSE <id>      Close an specific connection
+```
+
+Available commands in session context:
+````commandline
+COMMAND         DESCRIPTION
+------------------------------------
+STOP            Stop interacting with the current session
+CLOSE           Close the current connection
+````
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request. You can also simply open an issue with the tag "enhancement".
+Don't forget to give the project a star! Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the GNU General Public License v3.0. See `LICENSE` for more information.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
+
+<!-- LEGAL DISCLAIMER -->
+## Legal disclaimer
+This tool is created for the sole purpose of security awareness and education, it should not be used against systems that you do not have permission to test/attack. The author is not responsible for misuse or for any damage that you may cause. You agree that you use this software at your own risk.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+
---
+++ mc.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# https://github.com/r3nt0n/multicat
+# multicat - multithread reverse shell listener
+
+import socket, threading, time, sys, argparse
+
+name = 'multicat'
+desc = 'Multithread reverse shell listener'
+__version__ = '0.1.1'
+__author__ = 'r3nt0n'
+__status__ = 'Development'
+
+def process_args():
+    parser = argparse.ArgumentParser(description=desc)
+
+    parser.add_argument('-p', '--port', action="store", metavar='', type=int, dest='port',
+                        help='port to listen (default: 28000)', default=28000)
+    parser.add_argument('-m', '--max-clients', action="store", metavar='', type=int, dest='max_clients',
+                        default=5, help='max number of new clients to queue before establish connection (default: 5)')
+    parser.add_argument('-t', '--timeout', action="store", metavar='', type=int, dest='timeout',
+                        default=10, help='connections timeout (default: 10)')
+    args = parser.parse_args()
+    return args.port, args.max_clients, args.timeout
+
+class color:
+    PURPLE = u'\033[95m'
+    CYAN = u'\033[96m'
+    DARKCYAN = u'\033[36m'
+    BLUE = u'\033[94m'
+    GREEN = u'\033[92m'
+    YELLOW = u'\033[93m'
+    RED = u'\033[91m'
+    BOLD = u'\033[1m'
+    UNDERLINE = u'\033[4m'
+    ORANGE = u'\033[33m'
+    GREY = u'\033[90m'
+    END = u'\033[0m'
+
+
+class ThreadedServer(object):
+    def __init__(self, host, port, max_clients=5, timeout=10):
+        self.host = host
+        self.port = port
+        self.max_clients = max_clients
+        self.timeout = timeout
+        self.clients = []
+        self.current_session = ''
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.sock.bind((self.host, self.port))
+        super().__init__()
+
+    def listen(self):
+        self.sock.listen(self.max_clients)
+        print(f'{color.GREEN}[+]{color.END} Listening on port {color.GREEN}{color.BOLD}{self.port}{color.END}...\n')
+        while True:
+            client, address = self.sock.accept()
+            client.settimeout(self.timeout)
+            print(f'\n\n{color.GREEN}[+]{color.END} New client connected: %s:%d' % client.getpeername())
+            self.clients.append((client, address))
+
+    def listenToClient(self, client_object):
+        client = client_object[0]
+        address = client_object[1]
+        print(f'\n{color.GREEN}[*]{color.END} Connected to %s:%d' % client.getpeername())
+        buff_size = 1024
+        while True:
+            try:
+                recv_len = 1
+                output = b''
+                while recv_len:
+                    data = client.recv(buff_size)
+                    recv_len = len(data)
+                    output += data
+                    if recv_len < buff_size:
+                        break
+                try:
+                    print(output.decode('utf-8'), end='')
+                except UnicodeDecodeError:
+                    print(output.decode('latin-1'), end='')
+
+                #command = input('') + '\n'
+                command = input('')
+                if command.upper() in ('HELP', '?'):
+                    print(f'\n{color.BOLD}COMMAND\t\tDESCRIPTION{color.END}')
+                    print(f'------------------------------------')
+                    print('STOP\t\tStop interacting with the current session')
+                    print('CLOSE \t\tClose the current connection')
+                    command = ''
+                #if command.rstrip('\n').upper() == 'STOP':
+                if command.upper() == 'STOP':
+                    if input(f'\n{color.ORANGE}[?]{color.END} Do you want to {color.ORANGE}stop{color.END} this session? [y/N] ').lower() == 'y':
+                        print()
+                        self.current_session = ''
+                        client.sendall('\n'.encode())
+                        break
+                if command.upper() == 'CLOSE':
+                    if input(f'\n{color.RED}[?]{color.END} Do you want to {color.RED}close{color.END} this connection? [y/N] ').lower() == 'y':
+                        # client.sendall(command.encode())
+                        client.close()
+                        del self.clients[self.current_session]
+                        self.current_session = ''
+                        break
+                    else:
+                        command = ''
+
+                client.sendall((command + '\n').encode())
+
+                time.sleep(0.1)
+
+            except (socket.timeout, BrokenPipeError):
+                print(f'{color.RED}[?]{color.END} Client %s:%d disconnected' % address)
+                client.close()
+                del self.clients[self.current_session]
+                self.current_session = ''
+                break
+
+            finally:
+                if self.current_session == '':
+                    self.menu()
+
+    def create_client_thread(self, client):
+        threading.Thread(target=self.listenToClient, args=(client,)).start()
+
+    def menu(self):
+        print(f'Type help or ? to list commands.\n')
+        new_session = self.current_session
+        while True:
+            while new_session == '':
+                user_input = input(f'{color.BLUE}[{len(self.clients)}]{color.END} > ')
+                if user_input.upper() in ('HELP', '?'):
+                    print(f'\n{color.BOLD}COMMAND\t\tDESCRIPTION{color.END}')
+                    print(f'------------------------------------')
+                    print('HELP\t\tList available commands')
+                    print('SESSIONS\tList established sessions')
+                    print('START <id>\tInteract with a client')
+                    print('CLOSE <id>\tClose an specific connection')
+                    print()
+                #elif user_input.upper().startswith('HELP'):
+
+                elif user_input.upper().startswith('SESSIONS'):
+                    print(f'\n{color.BOLD}ID\tRemote address\tRemote port{color.END}')
+                    print(f'------------------------------------')
+                    for client in self.clients:
+                        print(f'{self.clients.index(client)}\t{client[1][0]}\t{client[1][1]}')
+                    print()
+                if not (user_input.upper().startswith('START') or user_input.upper().startswith('CLOSE')):
+                    continue
+                else:
+                    new_session = user_input.split()[-1]
+            try:
+                new_session = int(new_session)
+                if new_session+1 > len(self.clients):
+                    raise TypeError()
+            except (TypeError, IndexError, ValueError):
+                print(f'{color.RED}[!]{color.END} ERROR: You have entered a non-valid session ID')
+                new_session = ''
+                continue
+            if new_session != self.current_session:
+                if user_input.upper().startswith('START'):
+                    self.create_client_thread(self.clients[new_session])
+                    self.current_session = new_session
+                    break
+                elif user_input.upper().startswith('CLOSE'):
+                    self.clients[new_session][0].close()
+                    del self.clients[new_session]
+                    print(f'\n{color.ORANGE}[!]{color.END} Connection {new_session} closed\n')
+                    self.current_session = ''
+                    new_session = ''
+        return
+
+    def run(self):
+        threads = []
+        threads.append(threading.Thread(target=self.listen, args=()))
+        if self.current_session == '':
+            threads.append(threading.Thread(target=self.menu))
+        for thread in threads:
+            thread.start()
+        return
+
+if __name__ == "__main__":
+
+    print(f'\n{name} by r3nt0n - https://github.com/r3nt0n/multicat\n')
+    #
+    # if len(sys.argv) == 2:
+    #     port_num = sys.argv[1]
+    #     try:
+    #         port_num = int(port_num)
+    #         if port_num < 0:
+    #             raise ValueError
+    #     except ValueError:
+    #         print(f'{color.RED}[!]{color.END} ERROR: Invalid listening port')
+    #         sys.exit(1)
+        #ThreadedServer('', port_num).run()
+    port, max_clients, timeout = process_args()
+    ThreadedServer('', port, max_clients, timeout).run()
+    # else:
+    #     print(f'Usage:   {sys.argv[0]} <listening-port>')
+
